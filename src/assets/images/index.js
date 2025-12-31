d29c6c4ebad0cdd2e2610679fbfef64ef2729f3f
Emirhancelebi <emirhancelebi405@gmail.com>
2025-12-31 23:45:44 +0300

    shop düzenleme

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -16,6 +16,11 @@ import slide1 from "./slide1.png";
 import slide2 from "./slide2.jpg";
 import women from "./women.png";
 import fluid from "./fluid.png";
+import shopima from "./shopima.jpg"
+import shopima1 from "./shopima1.jpg"
+import shopima2 from "./shopima2.jpg"
+import shopima3 from "./shopima3.png"
+import shopima4 from "./shopima4.jpg"
 
 
 export {
@@ -26,7 +31,7 @@ export {
     sapka,
     reddit,
     stripe,
-    kids, men, post, slide1, slide2, women, accessories, fluid,
+    kids, men, post, slide1, slide2, women, accessories, fluid, shopima, shopima1, shopima2, shopima3, shopima4,
 };
 
 
@@ -37,4 +42,12 @@ export const brandImages = [
     { id: 4, src: sapka, alt: "Brand" },
     { id: 5, src: lyft, alt: "Lyft" },
     { id: 6, src: reddit, alt: "Reddit" },
+];
+export const clothImages = [
+    { id: 1, src: shopima, alt: "shopima" },
+    { id: 2, src: shopima1, alt: "shopima1" },
+    { id: 3, src: shopima2, alt: "shopima2" },
+    { id: 4, src: shopima3, alt: "shopima3" },
+    { id: 5, src: shopima4, alt: "shopima4" },
+
 ];
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "../layout/Header.jsx"
 import Footer from '../layout/Footer.jsx'
-import { product, brandImages } from "@images";
+import { product, clothImages, brandImages } from "@images";
 import ProductCard from '../components/ProductCard.jsx';
 
 
@@ -22,6 +22,25 @@ function ShopPage() {
                     <span className='text-h6li'>Home {">"} Shop</span>
                 </section>
 
+                <div className='flex flex-col items-center lg:flex-row pb-12 gap-2'>
+
+                    {clothImages.map((item) => (
+                        <div key={item.id} className='relative w-[332px] h-[300px] lg:w-[205px] lg:h-[223px] '>
+
+                            <img className='filter brightness-75 w-full h-full object-cover'
+
+                                src={item.src}
+                                alt={item.alt}
+                                loading="lazy"
+                            />
+                            <div className='absolute inset-0 flex flex-col items-center justify-center gap-1 '>
+                                <h5 className='text-light text-h5 '>Cloths</h5>
+                                <p className='text-light text-p'>5 Items</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
 
                 <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[30px]">
                     {products.map((item, index) => (
@@ -37,6 +56,15 @@ function ShopPage() {
 
                     ))}
                 </div>
+                <div className='container flex flex-row  justify-center h-18 py-10  '>
+                    <button className='rounded border-[#BDBDBD] border text-h6li w-[84px] h-[74px] bg-[#F3F3F3] text-[#BDBDBD] '>First</button>
+                    <button className='rounded border-[#BDBDBD] border text-h6li w-[46px] h-[74px] text-blu' >1</button>
+                    <button className='rounded border-[#BDBDBD] border text-h6li w-[46px] h-[74px] bg-blu text-light'>2</button>
+                    <button className='rounded border-[#BDBDBD] border text-h6li w-[46px] h-[74px] text-blu'>3</button>
+                    <button className='rounded border-[#BDBDBD] border text-h6li w-[84px] h-[74px] text-blu'>Next</button>
+
+                </div>
+
 
                 <section className=' container flex flex-col justify-between items-center  py-[50px] gap-[60px] lg:flex-row lg:py-12.5 lg:gap-[30px] '>
 
@@ -52,7 +80,10 @@ function ShopPage() {
                     </div>
 
 
+
                 </section>
+
+
 
 
 
